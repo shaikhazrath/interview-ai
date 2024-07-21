bbd4f9bad32d867bf2a97671542b3cf8d5a97c02
shaikhazrath <shaikhazrathali123@gmail.com>
2024-07-21 17:23:40 +0530

    fs fixed

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,15 @@
+// next.config.js
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  webpack: (config, { isServer }) => {
+    if (!isServer) {
+      config.resolve.fallback = {
+        fs: false, // Ignore `fs` module
+        path: false, // Add other Node.js modules if needed
+      };
+    }
+    return config;
+  },
+};
 
 export default nextConfig;

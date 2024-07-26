4b4efa38dba335c24265391f3e5179cbc77ba586
shaikhazrath <shaikhazrathali123@gmail.com>
2024-07-26 21:13:55 +0530

    vercel added

--- app/layout.js
+++ app/layout.js
@@ -15,7 +15,8 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en" className="">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>{children}        <Analytics />
+      </body>
       
 
     </html>
